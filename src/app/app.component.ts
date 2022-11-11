45a9f52ca1cacf67302e7e284aceb1da3ec3a8ae
nuruzi <39434080+nuruzi@users.noreply.github.com>
2022-11-11 12:39:12 +0330

    comment about mergmap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,22 +6,26 @@ import { share, switchMap, mergeMap, map } from 'rxjs/operators';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  getPosts = () =>
+    this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
 
-  getPosts = () => this.httpClient.get('https://jsonplaceholder.typicode.com/posts');
-
-  getComments = (post) => this
-    .httpClient
-    .get(`https://jsonplaceholder.typicode.com/posts/${post[0].id}/comments`);
-
-  getCommentsWithPost = post =>
-    this.getComments(post).pipe(
-      map(comments => ({ post, comments }))
+  getComments = (post) =>
+    this.httpClient.get(
+      `https://jsonplaceholder.typicode.com/posts/${post[0].id}/comments`
     );
 
-  c$: Observable<any> = this.getPosts().pipe(mergeMap(this.getCommentsWithPost));
+  getCommentsWithPost = (post) =>
+    this.getComments(post).pipe(map((comments) => ({ post, comments })));
 
-  constructor(private httpClient: HttpClient) { }
+  // get the information of the second service by the result of the first service
+  // اتصال سلسله مراتبی سرویس ها به وسیله mergmap
+  // Hierarchical connection of services by mergmap
+  c$: Observable<any> = this.getPosts().pipe(
+    mergeMap(this.getCommentsWithPost)
+  );
+
+  constructor(private httpClient: HttpClient) {}
 }
